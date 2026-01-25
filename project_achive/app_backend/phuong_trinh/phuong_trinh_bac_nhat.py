e541aafe50b22dd430d030737da8d79650db8248
Phongphan <binbonluc2011@gmail.com>
2026-01-25 21:59:55 +0700

    làm thêm 1 tí

--- project_achive/app_backend/phuong_trinh/phuong_trinh_bac_nhat.py
+++ project_achive/app_backend/phuong_trinh/phuong_trinh_bac_nhat.py
@@ -1,2 +1,16 @@
 import tkinter as tk
 from sympy import symbols, Eq, solve
+
+root = tk.Tk()
+root.state("zoomed")
+
+x = symbols("x")
+
+def PT_bac_nhat():
+    #tạo frame phương trình bậc nhất
+    pt_bac_nhat = tk.Frame(root, width=root.winfo_screenwidth()/2, bg="lightblue")
+    pt_bac_nhat.pack(side="left", fill="y")
+    #tạo tạo title phương trình bậc nhất
+    pt_title = tk.Label(pt_bac_nhat, text="PHƯƠNG TRÌNH BẬC NHẤT", font=("Arial", 16), bg="white")
+    pt_title.place(relx=0.5)
+    #tạo label phương trình bậc nhất
